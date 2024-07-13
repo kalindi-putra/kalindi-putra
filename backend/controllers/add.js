e0411b95d540e4f0140c27c785239bf776cfcd5c
divyeshpandey47@gmail.com <divyeshpandey47@gmail.com>
2024-07-14 02:00:02 +0530

    changes4

--- backend/controllers/add.js
+++ backend/controllers/add.js
@@ -3,6 +3,27 @@ const router = express.Router();
 const model = require('../model/user');
 const bcrypt=require('bcrypt')
 
+
+function checkPasswordStrength(password) {
+  // Regex patterns
+  const lengthRegex = /^.{6,10}$/;        // Match length between 6 to 10 characters
+  const letterRegex = /[a-zA-Z]/;         // Match at least one letter
+  const numberRegex = /\d/;               // Match at least one digit
+  const specialCharRegex = /[\W_]/;       // Match at least one special character (non-alphanumeric)
+
+  if (
+      lengthRegex.test(password) &&
+      letterRegex.test(password) &&
+      numberRegex.test(password) &&
+      specialCharRegex.test(password)
+  ) {
+      return true;
+  } else {
+      return false;
+  }
+}
+
+
 router.post('/',async (req,res)=>{
     try{
        // const check=await model.findAll({where:{phone:req.body.phone}});
@@ -12,9 +33,8 @@ router.post('/',async (req,res)=>{
        console.log(name,email,pass);
        if(name===undefined || email===undefined || pass===undefined)
        {
-        console.log(1);
          console.log(req.body);
-         return res.status(204).json("Pls fill all values");
+         return res.status(204).json({message:"Pls fill all values"});
        }
 
       const exist_user=await model.findOne({
@@ -27,11 +47,16 @@ router.post('/',async (req,res)=>{
 
         console.log(2);
         console.log(exist_user);
-             return res.status(201).json('User already exists');
+             return res.status(201).json({message:'User already exists'});
        }
 
-        //using bcrypt for passwords
-       // const salt=10;
+      if(!checkPasswordStrength(pass))
+      {
+        return res.status(201).json({message:"Password not according to format"})
+      }
+
+        
+
         const gensalt=await bcrypt.genSalt(2);
 
         const userPassword= await bcrypt.hash(pass,gensalt);
