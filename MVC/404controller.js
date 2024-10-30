c7a688d42f1f6d3f2cdc978550fca57e54365bff
Divyesh Pandey <94968285+kalindi-putra@users.noreply.github.com>
2024-10-31 00:09:51 +0530

    Delete MVC directory

--- MVC/404controller.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-const rootDir = require('../util/path');
-
-exports.error=app.use((req, res, next) => 
-{
-    res.status(404).sendFile(path.join(__dirname, '../views', '404.html'));
-});
--- MVC/contactcontroll.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const rootDir = require('../util/path');
-
-exports.getcontact=(req, res, next) => {
-    res.sendFile(path.join(__dirname, '../views', 'contact_us.html'));
-  };
-exports.postcontact=(req,res,next)=>{
-    res.redirect('/');
-};
